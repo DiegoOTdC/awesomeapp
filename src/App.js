fd03584e93a57aced5026f6805e8b0356f639c0c
DiegoOTdC <studioartdiego@gmail.com>
2020-06-29 12:55:27 +0200

    updated Title component and added Pokemon component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,54 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar Power", "Tough Claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewto",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega Beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Title />
+        <Title content="Gotta Catch 'em All!" />
+        {all_pokemon.map((pokemon) => (
+          <Pokemon
+            name={pokemon.name}
+            weight={pokemon.weight}
+            awesome={pokemon.awesome}
+            terrifying={pokemon.terrifying}
+            abilities={pokemon.abilities}
+          />
+        ))}
       </main>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Internet</h1>
-        <p>^Logo turns the other way..</p>
-        <h6>Imadethathappen(⌐■-■ </h6>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's learn about react :D
-        </a>
-      </header>
     </div>
   );
 }
